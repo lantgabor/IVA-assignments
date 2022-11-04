f3fcbffe6e8801e6d191c7d3610564d5c6a0ee06
lantgabor <lant.gabor98@gmail.com>
2022-11-04 23:35:52 +0100

    adds variance to graph

--- 2/src/main.cpp
+++ 2/src/main.cpp
@@ -23,15 +23,16 @@ void historgram(Mat& src, Mat& hists)
     }
 }
 
-int Otsu(Mat& src, Mat& dst, Mat& hists)
+int Otsu(Mat& src, Mat& dst, Mat& hists, Mat& vars)
 {
+
     // reset dst image
     dst = Mat::zeros(src.rows, src.cols, CV_8UC1);
     int threshold = 0;
 
     size_t total = src.total();
     int histSize = 256;
-    float varMax = 0, sum = 0, sumB = 0, q1 = 0, q2 = 0, u1 = 0, u2 = 0;
+    float varMax = 0, sum = 0, sumB = 0, q1 = 0, q2 = 0;
 
     for (int i = 0; i < histSize; i++) {
         sum += i * hists.at<float>(i);
@@ -39,21 +40,25 @@ int Otsu(Mat& src, Mat& dst, Mat& hists)
 
     // Algorithm explained: http://www.ipol.im/pub/art/2016/158/article.pdf
     for (int i = 0; i < histSize; i++) {
-        q1 += hists.at<float>(i);
-        if (q1 == 0)
+        q1 += hists.at<float>(i); // q1(t) = Sum_0^t { P(i) }
+
+        if (q1 == 0) // skip early zeros
             continue;
 
-        q2 = total - q1;
-        if (q2 == 0)
+        q2 = total - q1; // 1 - q1 or q2(t) = Sum_t+1^255 { P(i) }
+
+        if (q2 == 0) // skip late zeros
             break;
 
-        sumB += i * hists.at<float>(i);
-        float m1 = sumB / q1;
-        float m2 = (sum - sumB) / q2;
+        sumB += i * hists.at<float>(i); // i * P(i)
+        float u1 = sumB / q1;
+        float u2 = (sum - sumB) / q2;
 
-        float betweenVariance = q1 * q2 * (m1 - m2) * (m1 - m2);
+        float betweenVariance = q1 * q2 * (u1 - u2) * (u1 - u2); // q1(t)[1-q1(t)][u1(t)-u2(t)]^2
 
-        if (betweenVariance > varMax) {
+        vars.at<float>(i) = betweenVariance;
+
+        if (betweenVariance > varMax) { // Find maximum variance
             varMax = betweenVariance;
             threshold = i;
         }
@@ -74,7 +79,7 @@ int Otsu(Mat& src, Mat& dst, Mat& hists)
 int main(int argc, char const* argv[])
 {
     // LOAD IMAGE
-    cv::CommandLineParser parser(argc, argv, "{@input |../data/julia.png|input image}");
+    cv::CommandLineParser parser(argc, argv, "{@input |../data/lena.png|input image}");
     String imageName = parser.get<String>("@input");
     std::string image_path = samples::findFile(imageName);
     Mat img = imread(image_path, 0);
@@ -92,7 +97,8 @@ int main(int argc, char const* argv[])
     // val = threshold(img, img_bw, 0, 255, THRESH_OTSU);
     // std::cout << "Otsu obtained value (opencv): " << val << std::endl;
 
-    val = Otsu(img, img_bw, hists);
+    Mat vars = Mat::zeros(1, 256, CV_32FC1);
+    val = Otsu(img, img_bw, hists, vars);
     std::cout << "Otsu obtained value: " << val << std::endl;
 
     imshow("img_bw", img_bw);
@@ -101,13 +107,18 @@ int main(int argc, char const* argv[])
     int hist_w = 512, hist_h = 512;
     int bin_w = cvRound((double)hist_w / 256);
     Mat histsNorm;
+    Mat varsNorm;
     Mat histogram(hist_h, hist_w, CV_8UC3, Scalar(0));
     normalize(hists, histsNorm, 0, histogram.rows, NORM_MINMAX, -1, Mat());
+    normalize(vars, varsNorm, 0, histogram.rows, NORM_MINMAX, -1, Mat());
 
     for (int i = 1; i < 256; i++) {
         line(histogram, Point(bin_w * (i - 1), hist_h - cvRound(histsNorm.at<float>(i - 1))),
             Point(bin_w * (i), hist_h - cvRound(histsNorm.at<float>(i))),
             Scalar(255, 0, 0), 1, 8, 0);
+        line(histogram, Point(bin_w * (i - 1), hist_h - cvRound(varsNorm.at<float>(i - 1))),
+            Point(bin_w * (i), hist_h - cvRound(varsNorm.at<float>(i))),
+            Scalar(0, 0, 255), 1, 8, 0);
     }
     line(histogram, Point(bin_w * (val), hist_h), Point(bin_w * (val), 0), Scalar(0, 255, 0), 1, 8, 0);
 
