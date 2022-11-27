780f7215c5fc796521d01409ab20c5fe85f766c3
lantgabor <lant.gabor98@gmail.com>
2022-11-27 02:11:55 +0100

    with 360 circles

--- 3/src/main.cpp
+++ 3/src/main.cpp
@@ -10,21 +10,27 @@
 
 using namespace cv;
 using namespace std;
-void voteWithBoundaries(Mat& A, int x, int y)
+
+void voteWithBoundaries(Mat& A, int x, int y, int r, int rows, int cols, int min, int max)
 {
-    if (x < 0 || x >= A.rows || y < 0 || y >= A.cols)
+    if (x < 0 || x >= rows || y < 0 || y >= cols)
         return;
     else
-        A.at<uchar>(x, y) += 1;
-}
-void voteCircle(Mat& A, int x, int y, int r, float angle)
-{
-    voteWithBoundaries(A, x, y + r);
-    voteWithBoundaries(A, x, y - r);
-    voteWithBoundaries(A, x + r, y);
-    voteWithBoundaries(A, x - r, y);
+        A.at<uchar>(x, y, r - min) += 1;
 }
 
+void voteCircle(Mat& A, int x, int y, int r, float angle, int rows, int cols, int min, int max)
+{
+    voteWithBoundaries(A, x, y, r, rows, cols, min, max);
+    int a, b;
+    for (int t = 0; t < 360; t++) {
+        a = x + r * cos(t * CV_PI / 180);
+        b = y + r * sin(t * CV_PI / 180);
+        voteWithBoundaries(A, a, b, r, rows, cols, min, max);
+    }
+}
+
+// https://en.wikipedia.org/wiki/Circle_Hough_Transform
 void HC(Mat& gray, vector<Vec3f>& circles, int min, int max)
 {
 
@@ -39,55 +45,51 @@ void HC(Mat& gray, vector<Vec3f>& circles, int min, int max)
     Sobel(gray, dy, CV_32FC1, 0, 1);
     Mat angles;
 
-    phase(dx, dy, angles, true);
+    phase(dx, dy, angles, false);
     // angles = angles / 360 * 255;
     // angles.convertTo(angles, CV_8U);
 
     // 3D accumulator A[a,b,r] - cones
     // Init zero
-    vector<Mat> accumulator(max - min, Mat(gray.rows, gray.cols, CV_8UC1, Scalar(0)));
+    int sizes[] = { gray.rows, gray.cols, max - min };
+    Mat A(3, sizes, CV_8UC1, Scalar(0));
 
     for (int r = min; r < max; r++) {
-
-        // Get radius scaled to min-max
-        Mat& A = accumulator[r - min];
 
         for (int x = 0; x < edges.rows; x++) {
             for (int y = 0; y < edges.cols; y++) {
                 if (edges.at<uchar>(x, y) > 0) {
-                    voteCircle(A, x, y, r, angles.at<float>(x, y));
+                    voteCircle(A, x, y, r, angles.at<float>(x, y), edges.rows, edges.cols, min, max);
                 }
-                // Mat tmp = Mat::zeros(edges.rows, edges.cols, CV_8UC1);
-                // circle(tmp, Point(y, x), r, 1, 1);
-                // addWeighted(A, 1.0, tmp, 1.0, 0, A);
+            }
+        }
+
+        for (int x = 0; x < edges.rows; x++) {
+            for (int y = 0; y < edges.cols; y++) {
+                if (A.at<uchar>(x, y, r - min) > 155) {
+                    circles.push_back(Vec3f(y, x, r));
+                }
             }
         }
     }
 
-    // Mat accumulator = Mat::zeros(edges.rows, edges.cols, CV_8UC3);
-
-    // for (int r = min; r < max; r++) {
-    //     for (int x = 0; x < edges.rows; x++) {
-    //         for (int y = 0; y < edges.cols; y++) {
-    //             if (edges.at<uchar>(x, y) > 0) {
-    //
-    //
-    //
-    //             }
-    //         }
-    //     }
-    // }
-
-    // check_data(accumulator, "accumulator");
-    imshow("accumulator0", accumulator[20]);
+    for (size_t r = min; r < max; r += 10) {
+        Mat test = Mat::zeros(gray.rows, gray.cols, CV_8UC1);
+        for (int x = 0; x < gray.rows; x++) {
+            for (int y = 0; y < gray.cols; y++) {
+                test.at<uchar>(x, y) = A.at<uchar>(x, y, r - min);
+            }
+        }
+        imshow("test" + r, test);
+    }
 }
 
 int main(int argc, char const* argv[])
 {
     // LOAD IMAGE
-    cv::String keys = "{@input |../data/cells.png|input image}"
-                      "{@min |4 |min range of diameters}"
-                      "{@max |32 |max range of diameters}";
+    cv::String keys = "{@input |../data/cable.png|input image}"
+                      "{@min |32 |min range of diameters}"
+                      "{@max |128 |max range of diameters}";
 
     cv::CommandLineParser parser(argc, argv, keys);
 
@@ -103,13 +105,13 @@ int main(int argc, char const* argv[])
     Mat gray;
     cvtColor(img, gray, COLOR_BGR2GRAY);
     // medianBlur(gray, gray, 5);
-
     vector<Vec3f> circles;
-    HoughCircles(gray, circles, HOUGH_GRADIENT, 1,
-        img.rows / 16, // change this value to detect circles with different distances to each other
-        100, 30, parser.get<int>("@min"), parser.get<int>("@max") // change the last two parameters
-        // (min_radius & max_radius) to detect larger circles
-        );
+
+    // HoughCircles(gray, circles, HOUGH_GRADIENT, 1,
+    //     img.rows / 16, // change this value to detect circles with different distances to each other
+    //     100, 30, parser.get<int>("@min"), parser.get<int>("@max") // change the last two parameters
+    //     // (min_radius & max_radius) to detect larger circles
+    //     );
 
     HC(gray, circles, parser.get<int>("@min"), parser.get<int>("@max"));
 
