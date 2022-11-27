d80311d064ea7d2cfcbbdcd075cc204fa6ea0fb0
lantgabor <lant.gabor98@gmail.com>
2022-11-27 02:45:55 +0100

    finish

--- 3/src/main.cpp
+++ 3/src/main.cpp
@@ -21,40 +21,47 @@ void voteWithBoundaries(Mat& A, int x, int y, int r, int rows, int cols, int min
 
 void voteCircle(Mat& A, int x, int y, int r, float angle, int rows, int cols, int min, int max)
 {
+
+    // Try only with perpendicular points only
     voteWithBoundaries(A, x, y, r, rows, cols, min, max);
     int a, b;
-    for (int t = 0; t < 360; t++) {
-        a = x + r * cos(t * CV_PI / 180);
-        b = y + r * sin(t * CV_PI / 180);
-        voteWithBoundaries(A, a, b, r, rows, cols, min, max);
-    }
+    angle = angle - 90;
+    a = x + r * cos(angle * CV_PI / 180);
+    b = y - r * sin(angle * CV_PI / 180);
+    voteWithBoundaries(A, a, b, r, rows, cols, min, max);
+
+    a = x - r * cos(angle * CV_PI / 180);
+    b = y + r * sin(angle * CV_PI / 180);
+    voteWithBoundaries(A, a, b, r, rows, cols, min, max);
+
+    // Try with full circles
+    // for (int t = 0; t < 360; t++) {
+    //     a = x + r * cos(t * CV_PI / 180);
+    //     b = y + r * sin(t * CV_PI / 180);
+    //     voteWithBoundaries(A, a, b, r, rows, cols, min, max);
+    // }
 }
 
 // https://en.wikipedia.org/wiki/Circle_Hough_Transform
-void HC(Mat& gray, vector<Vec3f>& circles, int min, int max)
+void HC(Mat& gray, vector<Vec3f>& circles, int minDist, int param1, int param2, int min, int max)
 {
 
     // Edge detection
     Mat edges;
-    Canny(gray, edges, 50, 200, 3);
+    Canny(gray, edges, param1 / 2, param1, 3);
 
-    imshow("canny", edges);
-
+    // Get angles using Sobel
     Mat dx, dy;
     Sobel(gray, dx, CV_32FC1, 1, 0);
     Sobel(gray, dy, CV_32FC1, 0, 1);
     Mat angles;
-
-    phase(dx, dy, angles, false);
-    // angles = angles / 360 * 255;
-    // angles.convertTo(angles, CV_8U);
+    phase(dx, dy, angles, true);
 
     // 3D accumulator A[a,b,r] - cones
-    // Init zero
     int sizes[] = { gray.rows, gray.cols, max - min };
     Mat A(3, sizes, CV_8UC1, Scalar(0));
 
-    for (int r = min; r < max; r++) {
+    for (int r = min; r < max; r += minDist) { // radii in range [min-max]
 
         for (int x = 0; x < edges.rows; x++) {
             for (int y = 0; y < edges.cols; y++) {
@@ -64,15 +71,17 @@ void HC(Mat& gray, vector<Vec3f>& circles, int min, int max)
             }
         }
 
+        // Add center to cirlces if for all above the threshold
         for (int x = 0; x < edges.rows; x++) {
             for (int y = 0; y < edges.cols; y++) {
-                if (A.at<uchar>(x, y, r - min) > 155) {
+                if (A.at<uchar>(x, y, r - min) > param2) {
                     circles.push_back(Vec3f(y, x, r));
                 }
             }
         }
     }
 
+    // Display some accumulators
     for (size_t r = min; r < max; r += 10) {
         Mat test = Mat::zeros(gray.rows, gray.cols, CV_8UC1);
         for (int x = 0; x < gray.rows; x++) {
@@ -87,9 +96,9 @@ void HC(Mat& gray, vector<Vec3f>& circles, int min, int max)
 int main(int argc, char const* argv[])
 {
     // LOAD IMAGE
-    cv::String keys = "{@input |../data/cable.png|input image}"
-                      "{@min |32 |min range of diameters}"
-                      "{@max |128 |max range of diameters}";
+    cv::String keys = "{@input |../data/circles.png|input image}"
+                      "{@min |5 |min range of diameters}"
+                      "{@max |32 |max range of diameters}";
 
     cv::CommandLineParser parser(argc, argv, keys);
 
@@ -107,14 +116,18 @@ int main(int argc, char const* argv[])
     // medianBlur(gray, gray, 5);
     vector<Vec3f> circles;
 
-    // HoughCircles(gray, circles, HOUGH_GRADIENT, 1,
-    //     img.rows / 16, // change this value to detect circles with different distances to each other
-    //     100, 30, parser.get<int>("@min"), parser.get<int>("@max") // change the last two parameters
-    //     // (min_radius & max_radius) to detect larger circles
-    //     );
+    /*
+    // OpenCV Refernece
+    HoughCircles(gray, circles, HOUGH_GRADIENT, 1,
+        img.rows / 16, // change this value to detect circles with different distances to each other
+        100, 30, parser.get<int>("@min"), parser.get<int>("@max") // change the last two parameters
+        // (min_radius & max_radius) to detect larger circles
+        );
+    */
 
-    HC(gray, circles, parser.get<int>("@min"), parser.get<int>("@max"));
+    HC(gray, circles, 1, 155, 15, parser.get<int>("@min"), parser.get<int>("@max"));
 
+    // Display cirlces and centers on original img
     for (size_t i = 0; i < circles.size(); i++) {
         Vec3i c = circles[i];
         Point center = Point(c[0], c[1]);
@@ -125,6 +138,7 @@ int main(int argc, char const* argv[])
         circle(img, center, radius, Scalar(255, 0, 255), 1, LINE_AA);
     }
     imshow("detected circles", img);
+
     int k = waitKey(0); // Wait for a keystroke in the window
     // if (k == 's') {
     //     imwrite(imageName + "-" + std::to_string(val) + "-save.png", img_bw);
